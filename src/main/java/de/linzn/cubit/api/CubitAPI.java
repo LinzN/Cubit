dcfbecbc22239b86fd1eebadee1df9dbfea5aeb8
Niklas Linz <niklas.linz@hotmail.com>
2019-02-18 14:46:29 +0100

    Update CubitAPI.java

--- src/main/java/de/linzn/cubit/api/CubitAPI.java
+++ src/main/java/de/linzn/cubit/api/CubitAPI.java
@@ -20,36 +20,36 @@ import org.bukkit.World;
 public class CubitAPI {
 
     /**
-     * Method getCubitLand
-     * Get cubitland for specific coordinates
-     *
-     * @param world World for this cubitland
-     * @param chunkX X-coordinates for chunk
-     * @param chunkZ Z-coordinates for chunk
-     * @return CubitLand. Can return null if no one found
-     */
+    * Method getCubitLand
+    * Get cubitland for specific coordinates
+    *
+    * @param world World for this cubitland
+    * @param chunkX X-coordinates for chunk
+    * @param chunkZ Z-coordinates for chunk
+    * @return CubitLand. Can return null if no one found
+    */
     public static CubitLand getCubitLand(World world, int chunkX, int chunkZ) {
         return CubitBukkitPlugin.inst().getRegionManager().praseRegionData(world, chunkX, chunkZ);
     }
 
     /**
-     * Method getCubitLand
-     * Get cubitland for specific location
-     *
-     * @param location Location for this cubitland
-     * @return CubitLand. Can return null if no one found
-     */
+    * Method getCubitLand
+    * Get cubitland for specific location
+    *
+    * @param location Location for this cubitland
+    * @return CubitLand. Can return null if no one found
+    */
     public static CubitLand getCubitLand(Location location) {
         return getCubitLand(location.getWorld(), location.getChunk().getX(), location.getChunk().getZ());
     }
 
     /**
-     * Method getCubitLand
-     * Get cubitland for specific chunk
-     *
-     * @param chunk Chunk for this cubitland
-     * @return CubitLand. Can return null if no one found
-     */
+    * Method getCubitLand
+    * Get cubitland for specific chunk
+    *
+    * @param chunk Chunk for this cubitland
+    * @return CubitLand. Can return null if no one found
+    */
     public static CubitLand getCubitLand(Chunk chunk) {
         return getCubitLand(chunk.getWorld(), chunk.getX(), chunk.getZ());
     }
