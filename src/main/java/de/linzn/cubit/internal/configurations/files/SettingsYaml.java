abc35b7ea253319a1985e4513d0d14955a2a16ab
Niklas Linz <niklas.linz@hotmail.com>
2019-07-29 11:36:24 +0200

    Update SettingsYaml.java

--- src/main/java/de/linzn/cubit/internal/configurations/files/SettingsYaml.java
+++ src/main/java/de/linzn/cubit/internal/configurations/files/SettingsYaml.java
@@ -85,7 +85,7 @@ public class SettingsYaml {
         this.landBuyMaterialBorder = Material
                 .valueOf((String) this.getObjectValue("module.land.buyMaterialBorder", Material.TORCH.toString()));
         this.landSellMaterialBorder = Material.valueOf(
-                (String) this.getObjectValue("module.land.sellMaterialBorder", Material.REDSTONE_TORCH.toString()));
+                (String) this.getObjectValue("module.land.sellMaterialBorder", Material.REDSTONE_TORCH_ON.toString()));
         this.landUseScoreboardMap = (boolean) this.getObjectValue("module.land.useScoreboardMap", false);
 
         List<String> shopsEnabledWorlds = new ArrayList<>();
@@ -103,8 +103,8 @@ public class SettingsYaml {
         this.shopMaterialCleanup = new ArrayList<>();
 
         List<String> shopCleanupMaterial = new ArrayList<>();
-        shopCleanupMaterial.add(Material.LEGACY_SIGN.toString());
-        shopCleanupMaterial.add(Material.LEGACY_WALL_SIGN.toString());
+        shopCleanupMaterial.add(Material.SIGN.toString());
+        shopCleanupMaterial.add(Material.WALL_SIGN.toString());
 
         for (String materialEntry : (List<String>) this.getStringList("module.shop.shopMaterialCleanup", shopCleanupMaterial)) {
             this.shopMaterialCleanup.add(Material.valueOf(materialEntry));
