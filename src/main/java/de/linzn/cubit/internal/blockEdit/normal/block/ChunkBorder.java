3b7549b813e96dff236e0e8a5ac8796959c3fc18
Niklas Linz <niklas.linz@hotmail.com>
2019-07-29 11:47:33 +0200

    Update src/main/java/de/linzn/cubit/internal/blockEdit/normal/block/ChunkBorderCleaner.java, src/main/java/de/linzn/cubit/internal/blockEdit/normal/block/ChunkBorder.java files

--- src/main/java/de/linzn/cubit/internal/blockEdit/normal/block/ChunkBorder.java
+++ src/main/java/de/linzn/cubit/internal/blockEdit/normal/block/ChunkBorder.java
@@ -63,7 +63,7 @@ public class ChunkBorder implements Runnable {
         int y;
         for (y = 255; y > 0; y--) {
             if ((world.getBlockAt(x, y, z).getType() != Material.AIR)
-                    && (world.getBlockAt(x, y, z).getType() != Material.END_PORTAL)
+                    && (world.getBlockAt(x, y, z).getType() != Material.ENDER_PORTAL_FRAME)
                     && (world.getBlockAt(x, y, z).getType() != Material.BEDROCK)) {
                 if ((world.getBlockAt(x, y, z).getType() != Material.TORCH)
                         && (world.getBlockAt(x, y, z).getType() != Material.REDSTONE_TORCH_ON)
